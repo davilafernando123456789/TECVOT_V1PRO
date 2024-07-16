458505c699a5a2d91e654d24bb077801715392b3
cintia <cintia.maucaylle@tecsup.edu.pe>
2024-07-16 11:05:29 -0500

    tercer comiit aui

--- BACKEND-TECVOC-1/config/db.js
+++ BACKEND-TECVOC-1/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const conectarMongoDB = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/MiBasedeDatos', {
+        await mongoose.connect('mongodb://127.0.0.1:27017/miBasedeDatos', {
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
